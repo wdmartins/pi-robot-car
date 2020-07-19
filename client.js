f444c38eb4d41232451dabb9fecff5d14a7776db
Walter Martins <wdmartins@gmail.com>
2020-07-19 15:57:20 -0400

    WIP: Cleaned up client code

--- client.js
+++ client.js
@@ -1,106 +1,109 @@
 'use strict';
+
+//-------------------------------------------------------------------------------------------------------
+// Vue.js setup
+//-------------------------------------------------------------------------------------------------------
 let app;
-function initUI (onCallUserCb, onSearchStringUpdated, onTranscriptionStarted) {
-    app = new Vue({
-        el: "#app",
-        methods: {
-            placeholder: function() {
-            },
-            setAudioSource: function (source) {
-                this.audioElement.srcObject = source;
-            }
-        },
-        data: {
-            title: 'Title',
-            date: '',
-            time: '',
-        }
-    });
-    return;
+const data = {
+    speechRecognitionResult: {
+        label: '',
+        confidence: ''
+    }
 }
-initUI();
-console.log('ml5 version:', ml5.version);
-// Initialize a sound classifier method with SpeechCommands18w model. A callback needs to be passed.
-let classifier;
-// Options for the SpeechCommands18w model, the default probabilityThreshold is 0
-const options = { probabilityThreshold: 0.97 };
-// Two variable to hold the label and confidence of the result
-let label;
-let confidence;
+app = new Vue({
+    el: "#app",
+    data
+});
+
+//-------------------------------------------------------------------------------------------------------
+// Instatiate a websocket object 
+//-------------------------------------------------------------------------------------------------------
+const socket = io({
+    transports: ['websocket']
+});
 
 
+//-------------------------------------------------------------------------------------------------------
+// Speech Recognition handling
+//-------------------------------------------------------------------------------------------------------
 
+// Process results from ml5 sound recognition
+function gotResult(error, results) {
+    // Display error in the console
+    if (error) {
+      console.error(error);
+    }
+    // The results are in an array ordered by confidence.
+    const result = {
+      label: results[0].label,
+      confidence: results[0].confidence.toFixed(4)
+    };
+
+    const command = {
+        command: result.label,
+        confidence: result.confidence
+    };
+    socket.emit('command', command);
+    
+    console.log('Send command: ', command);
+    app.speechRecognitionResult = result;
+}
+
+// Setup ml5
 async function setup() {
-  classifier = await ml5.soundClassifier('SpeechCommands18w', options);
-  // Create 'label' and 'confidence' div to hold results
+    // Initialize a sound classifier method with SpeechCommands18w model. A callback needs to be passed.
+    const options = { probabilityThreshold: 0.97 };
+    const classifier = await ml5.soundClassifier('SpeechCommands18w', options);
   
-  label = document.createElement('DIV');
-  label.textContent = 'label ...';
-  confidence = document.createElement('DIV');
-  confidence.textContent = 'Confidence ...';
-
-  document.body.appendChild(label);
-  document.body.appendChild(confidence);
-  // Classify the sound from microphone in real time
-  classifier.classify(gotResult);
+    // Classify the sound from microphone in real time
+    classifier.classify(gotResult);
 }
 setup();
 
+//-------------------------------------------------------------------------------------------------------
+// WebSocket Handling
+//-------------------------------------------------------------------------------------------------------
+let interval;
+let ping = 0;
 
-// A function to run when we get any errors and the results
-// var socket = io('https://pi-robot-car.duckdns.org');
-var socket = io({
-  transports: ['websocket']
-});
-var interval;
-var ping = 0;
-function gotResult(error, results) {
-  // Display error in the console
-  if (error) {
-    console.error(error);
-  }
-  // The results are in an array ordered by confidence.
-  //console.log(results);
-  // Show the first label and confidence
-  label.textContent = 'Label: ' + results[0].label;
-  confidence.textContent = 'Confidence: ' + results[0].confidence.toFixed(4);
-  socket.emit('command', {
-    command: results[0].label,
-    confidence: results[0].confidence.toFixed(4)
-  });
-  console.log(`Send command: ${label.textContent}`);
-}
+// Register for socket reconnection attempt
 socket.on('reconnect_attempt', () => {
   socket.io.opts.transports = ['polling', 'websocket'];
 });
+
+// Register for sockect connection
 socket.on('connect', function() {
-  console.log('Connected');
-  if (interval) {
-    clearInterval(interval);
-    interval = null;
-  }
-  interval = setInterval(() => {
-    console.log(`Sending PING ${ping}`);
-    socket.emit('PING', {
-      ping: ping++
-    });
-  }, 10000);
-});
-socket.on('PONG', (data) => {
-  console.log(`Received PONG ${data.pong}`);
-});
-socket.on('event', function(data) {
-  console.log('Data');
-});
-socket.on('info', function(data) {
-  console.log(`Info received: ${data}`);
+    console.log('Connected');
+    if (interval) {
+        clearInterval(interval);
+        interval = null;
+    }
+
+    // Start pinging the server every 10 seconds
+    interval = setInterval(() => {
+        console.log(`Sending PING ${ping}`);
+        socket.emit('PING', {
+            ping: ping++
+        });
+    }, 10000);
 });
 
+// Register for ping from the server
+socket.on('PONG', (data) => {
+    console.log(`Received PONG ${data.pong}`);
+});
+
+// Register for event received from the server
+socket.on('event', function(data) {
+    console.log('Data');
+});
+
+// Register for socket disconnection
 socket.on('disconnect', function() {
-  if (interval) {
-    clearInterval(interval);
-    interval = null;
-    ping = 0;
-  }
-  console.log('Disconnected')
+    if (interval) {
+        clearInterval(interval);
+        interval = null;
+        ping = 0;
+    }
+    console.log('Disconnected')
 });
